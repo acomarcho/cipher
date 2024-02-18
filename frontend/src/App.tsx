d80389bdb0cce15b4909fe1d768402119938c1e8
acomarcho <acoxstpd@gmail.com>
2024-02-18 21:03:56 +0700

    Feat: Slice input for keys

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
 import { RadioGroup, RadioGroupItem } from "./components/ui/radio-group";
 import {
@@ -62,13 +63,45 @@ const ciphers = [
   },
 ];
 
+type TextKey = string;
+type AffineKey = {
+  m: string;
+  b: string;
+};
+type HillKey = string[][];
+type SuperKey = {
+  vigenere: string;
+  transposition: string;
+};
+type CipherKey = TextKey | AffineKey | HillKey | SuperKey;
+
+const isTextKey = (_key: CipherKey, cipher: Cipher): _key is TextKey => {
+  return (
+    cipher === Cipher.AutoKeyVigenere ||
+    cipher === Cipher.ExtendedVigenere ||
+    cipher === Cipher.StandardVigenere ||
+    cipher === Cipher.Playfair
+  );
+};
+const isAffineKey = (_key: CipherKey, cipher: Cipher): _key is AffineKey => {
+  return cipher === Cipher.Affine;
+};
+const isHillKey = (_key: CipherKey, cipher: Cipher): _key is HillKey => {
+  return cipher === Cipher.Hill;
+};
+const isSuperKey = (_key: CipherKey, cipher: Cipher): _key is SuperKey => {
+  return cipher === Cipher.Super;
+};
+
 type CipherForm = {
   cipher: Cipher;
+  key: CipherKey;
 };
 
 const App = () => {
   const [form, setForm] = useState<CipherForm>({
     cipher: Cipher.StandardVigenere,
+    key: "",
   });
 
   const handleCipherChange = (v: string) => {
@@ -76,9 +109,49 @@ const App = () => {
       return;
     }
 
+    switch (v) {
+      case Cipher.Affine: {
+        setForm({
+          ...form,
+          cipher: v,
+          key: {
+            m: "",
+            b: "",
+          },
+        });
+        break;
+      }
+      case Cipher.Hill: {
+        setForm({
+          ...form,
+          cipher: v,
+          key: [],
+        });
+        break;
+      }
+      case Cipher.Super: {
+        setForm({
+          ...form,
+          cipher: v,
+          key: {
+            vigenere: "",
+            transposition: "",
+          },
+        });
+        break;
+      }
+      default: {
+        setForm({
+          ...form,
+          cipher: v,
+          key: "",
+        });
+      }
+    }
     setForm({
       ...form,
       cipher: v,
+      key: "",
     });
   };
 
@@ -90,7 +163,7 @@ const App = () => {
       <p className="leading-7 [&:not(:first-child)]:mt-6">
         Secure your file/texts by using our cipher technology!
       </p>
-      <div className="p-[2rem] border-2 border-black mt-6 rounded-xl">
+      <div className="p-[2rem] border-2 border-black mt-6 rounded-xl space-y-[2rem]">
         <div className="flex flex-col gap-[1rem]">
           <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
             Cipher
@@ -114,7 +187,70 @@ const App = () => {
             </SelectContent>
           </Select>
         </div>
-        <div className="flex flex-col gap-[1rem] mt-[2rem]">
+        <div className="flex flex-col gap-[1rem]">
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+            Key
+          </h3>
+          {isTextKey(form.key, form.cipher) && (
+            <Input
+              type="text"
+              placeholder="Enter cipher key ..."
+              value={form.key}
+            />
+          )}
+          {isAffineKey(form.key, form.cipher) && (
+            <>
+              <div className="flex flex-col gap-[1rem]">
+                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                  Value for m
+                </h4>
+                <Input
+                  type="number"
+                  placeholder="Enter value for m ..."
+                  value={form.key.m}
+                />
+              </div>
+              <div className="flex flex-col gap-[1rem]">
+                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                  Value for b
+                </h4>
+                <Input
+                  type="number"
+                  placeholder="Enter value for b ..."
+                  value={form.key.b}
+                />
+              </div>
+            </>
+          )}
+          {isHillKey(form.key, form.cipher) && (
+            <span>TODO - Implement Hill Key input</span>
+          )}
+          {isSuperKey(form.key, form.cipher) && (
+            <>
+              <div className="flex flex-col gap-[1rem]">
+                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                  Vigenere key
+                </h4>
+                <Input
+                  type="number"
+                  placeholder="Enter vigenere key ..."
+                  value={form.key.vigenere}
+                />
+              </div>
+              <div className="flex flex-col gap-[1rem]">
+                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                  Transposition key
+                </h4>
+                <Input
+                  type="number"
+                  placeholder="Enter transposition key ..."
+                  value={form.key.transposition}
+                />
+              </div>
+            </>
+          )}
+        </div>
+        <div className="flex flex-col gap-[1rem]">
           <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
             Input Type
           </h3>
