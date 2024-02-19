dd487c665b39e4b96455a7d03b5be0e4908e4442
acomarcho <acoxstpd@gmail.com>
2024-02-19 16:47:42 +0700

    Feat: Implement Affine decrypt (file)

--- backend/src/cipher/controller/hill.ts
+++ backend/src/cipher/controller/hill.ts
@@ -159,6 +159,72 @@ export class HillController {
           .json(new ApiResponse(null, error));
       }
     });
+
+    this.router.put(
+      "/hill/decrypt/file",
+      upload.single("file"),
+      async (req, res) => {
+        try {
+          if (!req.file) {
+            return res
+              .status(status.BAD_REQUEST)
+              .json(new ApiResponse(null, "File not given"));
+          }
+
+          if (!req.body.key) {
+            return res
+              .status(status.BAD_REQUEST)
+              .json(new ApiResponse(null, "Key not given"));
+          }
+
+          const toParse = {
+            key: JSON.parse(req.body.key),
+          };
+          const parsedRequest = fileHillRequestSchema.safeParse(toParse);
+          if (!parsedRequest.success) {
+            return res
+              .status(status.BAD_REQUEST)
+              .json(new ApiResponse(null, "Incomplete fields"));
+          }
+
+          const { key } = parsedRequest.data;
+          if (key.length !== key[0].length) {
+            return res
+              .status(status.BAD_REQUEST)
+              .json(new ApiResponse(null, "Key must be a square matrix"));
+          }
+          if (det(key) === 0) {
+            return res
+              .status(status.BAD_REQUEST)
+              .json(new ApiResponse(null, "Key must be an invertible matrix"));
+          }
+
+          const fileContents = await fs.readFile(req.file.path, "utf8");
+
+          const result = this.hillUseCase.decrypt({
+            cipherText: sanitizeInputAsAlphabetOnly(fileContents).toUpperCase(),
+            key: key,
+          });
+
+          await fs.writeFile(req.file.path, result.text, "utf-8");
+          const newFileContents = await fs.readFile(req.file.path);
+
+          const readStream = new stream.PassThrough();
+          readStream.end(newFileContents);
+
+          res.set(
+            "Content-Disposition",
+            "attachment; filename=" + req.file.originalname
+          );
+          res.set("Content-Type", "text/plain");
+          return readStream.pipe(res);
+        } catch (error) {
+          return res
+            .status(status.INTERNAL_SERVER_ERROR)
+            .json(new ApiResponse(null, error));
+        }
+      }
+    );
   }
 
   public getRouter = () => {
