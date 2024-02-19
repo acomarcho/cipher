3816ebfdb6acd347bbc42449a312e203a697f4b6
acomarcho <acoxstpd@gmail.com>
2024-02-19 13:47:37 +0700

    Refactor: useCipherForm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-import axios from "axios";
-import { useState } from "react";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
@@ -12,16 +10,13 @@ import {
   SelectValue,
 } from "./components/ui/select";
 import { Textarea } from "./components/ui/textarea";
+import { useCipherForm } from "./hooks/use-cipher-form";
 import {
-  BE_URL,
   Cipher,
-  CipherForm,
-  CipherResult,
   InputType,
   PageStatus,
   TextInputType,
   isAffineKey,
-  isCipher,
   isHillKey,
   isSuperKey,
   isTextKey,
@@ -60,311 +55,25 @@ const ciphers = [
 ];
 
 const App = () => {
-  const [form, setForm] = useState<CipherForm>({
-    cipher: Cipher.StandardVigenere,
-    key: "",
-    inputType: InputType.Text,
-    textInputType: TextInputType.Text,
-    textInput: "",
-  });
-
-  const [pageStatus, setPageStatus] = useState<PageStatus>(PageStatus.None);
-
-  const [cipherResult, setCipherResult] = useState<CipherResult | null>(null);
-
-  const handleCipherChange = (v: string) => {
-    if (!isCipher(v)) {
-      return;
-    }
-
-    switch (v) {
-      case Cipher.Affine: {
-        setForm({
-          ...form,
-          cipher: v,
-          key: {
-            m: "",
-            b: "",
-          },
-        });
-        break;
-      }
-      case Cipher.Hill: {
-        setForm({
-          ...form,
-          cipher: v,
-          key: {
-            matrixSize: "2",
-            matrix: [
-              ["", ""],
-              ["", ""],
-            ],
-          },
-        });
-        break;
-      }
-      case Cipher.Super: {
-        setForm({
-          ...form,
-          cipher: v,
-          key: {
-            vigenere: "",
-            transposition: "",
-          },
-        });
-        break;
-      }
-      default: {
-        setForm({
-          ...form,
-          cipher: v,
-          key: "",
-        });
-      }
-    }
-  };
-
-  const handleTextKeyChange = (v: string) => {
-    if (!isTextKey(form.key, form.cipher)) {
-      return;
-    }
-
-    setForm({
-      ...form,
-      key: v,
-    });
-  };
-
-  const handleAffineKeyMChange = (m: string) => {
-    if (!isAffineKey(form.key, form.cipher)) {
-      return;
-    }
-
-    setForm({
-      ...form,
-      key: {
-        ...form.key,
-        m: m,
-      },
-    });
-  };
-  const handleAffineKeyBChange = (b: string) => {
-    if (!isAffineKey(form.key, form.cipher)) {
-      return;
-    }
-
-    setForm({
-      ...form,
-      key: {
-        ...form.key,
-        b: b,
-      },
-    });
-  };
-
-  const handleSuperKeyVigenereChange = (vigenere: string) => {
-    if (!isSuperKey(form.key, form.cipher)) {
-      return;
-    }
-
-    setForm({
-      ...form,
-      key: {
-        ...form.key,
-        vigenere: vigenere,
-      },
-    });
-  };
-  const handleSuperKeyTranspositionChange = (transposition: string) => {
-    if (!isSuperKey(form.key, form.cipher)) {
-      return;
-    }
-
-    setForm({
-      ...form,
-      key: {
-        ...form.key,
-        transposition: transposition,
-      },
-    });
-  };
-
-  const handleHillKeyMatrixSizeChange = (size: string) => {
-    if (!isHillKey(form.key, form.cipher)) {
-      return;
-    }
-
-    if (parseInt(size) < 2) {
-      return;
-    }
-
-    const newMatrix: string[][] = [];
-    const newSize = parseInt(size);
-    for (let i = 0; i < newSize; i++) {
-      const newRow: string[] = [];
-      for (let j = 0; j < newSize; j++) {
-        newRow.push("");
-      }
-      newMatrix.push(newRow);
-    }
-
-    setForm({
-      ...form,
-      key: {
-        ...form.key,
-        matrixSize: size,
-        matrix: newMatrix,
-      },
-    });
-  };
-  const handleHillKeyMatrixChange = (
-    row: number,
-    col: number,
-    value: string
-  ) => {
-    if (!isHillKey(form.key, form.cipher)) {
-      return;
-    }
-
-    const newMatrix: string[][] = [];
-    const size = parseInt(form.key.matrixSize);
-    for (let i = 0; i < size; i++) {
-      const newRow: string[] = [];
-      for (let j = 0; j < size; j++) {
-        if (i === row && j === col) {
-          newRow.push(value);
-        } else {
-          newRow.push(form.key.matrix[i][j]);
-        }
-      }
-      newMatrix.push(newRow);
-    }
-
-    setForm({
-      ...form,
-      key: {
-        ...form.key,
-        matrix: newMatrix,
-      },
-    });
-  };
-
-  const handleInputTypeChange = (v: string) => {
-    setForm({
-      ...form,
-      inputType: v,
-    });
-  };
-
-  const handleTextInputTypeChange = (v: string) => {
-    setForm({
-      ...form,
-      textInputType: v,
-      textInput: "",
-    });
-  };
-
-  const handleTextInputChange = (v: string) => {
-    setForm({
-      ...form,
-      textInput: v,
-    });
-  };
-
-  const isKeyCompleted =
-    (isTextKey(form.key, form.cipher) && form.key !== "") ||
-    (isAffineKey(form.key, form.cipher) &&
-      form.key.m !== "" &&
-      form.key.b !== "") ||
-    (isHillKey(form.key, form.cipher) &&
-      form.key.matrix.filter((row) => {
-        const filteredRow = row.filter((entry) => entry === "");
-        return filteredRow.length > 0;
-      }).length === 0) ||
-    (isSuperKey(form.key, form.cipher) &&
-      form.key.vigenere !== "" &&
-      form.key.transposition !== "");
-
-  const isFormComplete =
-    isKeyCompleted &&
-    form.inputType === InputType.Text &&
-    form.textInput !== "";
-
-  const generateRequestKey = () => {
-    return isTextKey(form.key, form.cipher)
-      ? form.key
-      : isAffineKey(form.key, form.cipher)
-      ? {
-          m: parseInt(form.key.m),
-          b: parseInt(form.key.b),
-        }
-      : isSuperKey(form.key, form.cipher)
-      ? {
-          vigenere: form.key.vigenere,
-          transposition: parseInt(form.key.transposition),
-        }
-      : isHillKey(form.key, form.cipher)
-      ? form.key.matrix.map((row) => {
-          return row.map((entry) => parseInt(entry));
-        })
-      : "";
-  };
-
-  const handleEncryptClick = async () => {
-    try {
-      setCipherResult(null);
-      setPageStatus(PageStatus.Loading);
-
-      const textToSend =
-        form.textInputType === TextInputType.Text
-          ? form.textInput
-          : safeAtob(form.textInput);
-
-      const key = generateRequestKey();
-
-      const { data } = await axios.put<CipherResult>(
-        `${BE_URL}/cipher/${form.cipher}/encrypt/text`,
-        {
-          text: textToSend,
-          key,
-        }
-      );
-
-      setCipherResult(data);
-    } catch {
-      window.alert("Failed to perform encryption");
-    } finally {
-      setPageStatus(PageStatus.None);
-    }
-  };
-
-  const handleDecryptClick = async () => {
-    try {
-      setCipherResult(null);
-      setPageStatus(PageStatus.Loading);
-
-      const textToSend =
-        form.textInputType === TextInputType.Text
-          ? form.textInput
-          : safeAtob(form.textInput);
-
-      const key = generateRequestKey();
-
-      const { data } = await axios.put<CipherResult>(
-        `${BE_URL}/cipher/${form.cipher}/decrypt/text`,
-        {
-          text: textToSend,
-          key,
-        }
-      );
-
-      setCipherResult(data);
-    } catch {
-      window.alert("Failed to perform encryption");
-    } finally {
-      setPageStatus(PageStatus.None);
-    }
-  };
+  const {
+    form,
+    pageStatus,
+    cipherResult,
+    handleCipherChange,
+    handleTextKeyChange,
+    handleAffineKeyMChange,
+    handleAffineKeyBChange,
+    handleSuperKeyVigenereChange,
+    handleSuperKeyTranspositionChange,
+    handleHillKeyMatrixSizeChange,
+    handleHillKeyMatrixChange,
+    handleInputTypeChange,
+    handleTextInputTypeChange,
+    handleTextInputChange,
+    isFormComplete,
+    handleEncryptClick,
+    handleDecryptClick,
+  } = useCipherForm();
 
   return (
     <div className="max-w-[1160px] mx-auto p-[2rem]">
---
+++ frontend/src/hooks/use-cipher-form.ts
@@ -0,0 +1,345 @@
+import {
+  CipherForm,
+  InputType,
+  TextInputType,
+  PageStatus,
+  CipherResult,
+  isCipher,
+  isTextKey,
+  isAffineKey,
+  isSuperKey,
+  isHillKey,
+  BE_URL,
+  Cipher,
+} from "@/lib/constants";
+import { safeAtob } from "@/lib/utils";
+import axios from "axios";
+import { useState } from "react";
+
+export const useCipherForm = () => {
+  const [form, setForm] = useState<CipherForm>({
+    cipher: Cipher.StandardVigenere,
+    key: "",
+    inputType: InputType.Text,
+    textInputType: TextInputType.Text,
+    textInput: "",
+  });
+
+  const [pageStatus, setPageStatus] = useState<PageStatus>(PageStatus.None);
+
+  const [cipherResult, setCipherResult] = useState<CipherResult | null>(null);
+
+  const handleCipherChange = (v: string) => {
+    if (!isCipher(v)) {
+      return;
+    }
+
+    switch (v) {
+      case Cipher.Affine: {
+        setForm({
+          ...form,
+          cipher: v,
+          key: {
+            m: "",
+            b: "",
+          },
+        });
+        break;
+      }
+      case Cipher.Hill: {
+        setForm({
+          ...form,
+          cipher: v,
+          key: {
+            matrixSize: "2",
+            matrix: [
+              ["", ""],
+              ["", ""],
+            ],
+          },
+        });
+        break;
+      }
+      case Cipher.Super: {
+        setForm({
+          ...form,
+          cipher: v,
+          key: {
+            vigenere: "",
+            transposition: "",
+          },
+        });
+        break;
+      }
+      default: {
+        setForm({
+          ...form,
+          cipher: v,
+          key: "",
+        });
+      }
+    }
+  };
+
+  const handleTextKeyChange = (v: string) => {
+    if (!isTextKey(form.key, form.cipher)) {
+      return;
+    }
+
+    setForm({
+      ...form,
+      key: v,
+    });
+  };
+
+  const handleAffineKeyMChange = (m: string) => {
+    if (!isAffineKey(form.key, form.cipher)) {
+      return;
+    }
+
+    setForm({
+      ...form,
+      key: {
+        ...form.key,
+        m: m,
+      },
+    });
+  };
+  const handleAffineKeyBChange = (b: string) => {
+    if (!isAffineKey(form.key, form.cipher)) {
+      return;
+    }
+
+    setForm({
+      ...form,
+      key: {
+        ...form.key,
+        b: b,
+      },
+    });
+  };
+
+  const handleSuperKeyVigenereChange = (vigenere: string) => {
+    if (!isSuperKey(form.key, form.cipher)) {
+      return;
+    }
+
+    setForm({
+      ...form,
+      key: {
+        ...form.key,
+        vigenere: vigenere,
+      },
+    });
+  };
+  const handleSuperKeyTranspositionChange = (transposition: string) => {
+    if (!isSuperKey(form.key, form.cipher)) {
+      return;
+    }
+
+    setForm({
+      ...form,
+      key: {
+        ...form.key,
+        transposition: transposition,
+      },
+    });
+  };
+
+  const handleHillKeyMatrixSizeChange = (size: string) => {
+    if (!isHillKey(form.key, form.cipher)) {
+      return;
+    }
+
+    if (parseInt(size) < 2) {
+      return;
+    }
+
+    const newMatrix: string[][] = [];
+    const newSize = parseInt(size);
+    for (let i = 0; i < newSize; i++) {
+      const newRow: string[] = [];
+      for (let j = 0; j < newSize; j++) {
+        newRow.push("");
+      }
+      newMatrix.push(newRow);
+    }
+
+    setForm({
+      ...form,
+      key: {
+        ...form.key,
+        matrixSize: size,
+        matrix: newMatrix,
+      },
+    });
+  };
+  const handleHillKeyMatrixChange = (
+    row: number,
+    col: number,
+    value: string
+  ) => {
+    if (!isHillKey(form.key, form.cipher)) {
+      return;
+    }
+
+    const newMatrix: string[][] = [];
+    const size = parseInt(form.key.matrixSize);
+    for (let i = 0; i < size; i++) {
+      const newRow: string[] = [];
+      for (let j = 0; j < size; j++) {
+        if (i === row && j === col) {
+          newRow.push(value);
+        } else {
+          newRow.push(form.key.matrix[i][j]);
+        }
+      }
+      newMatrix.push(newRow);
+    }
+
+    setForm({
+      ...form,
+      key: {
+        ...form.key,
+        matrix: newMatrix,
+      },
+    });
+  };
+
+  const handleInputTypeChange = (v: string) => {
+    setForm({
+      ...form,
+      inputType: v,
+    });
+  };
+
+  const handleTextInputTypeChange = (v: string) => {
+    setForm({
+      ...form,
+      textInputType: v,
+      textInput: "",
+    });
+  };
+
+  const handleTextInputChange = (v: string) => {
+    setForm({
+      ...form,
+      textInput: v,
+    });
+  };
+
+  const isKeyCompleted =
+    (isTextKey(form.key, form.cipher) && form.key !== "") ||
+    (isAffineKey(form.key, form.cipher) &&
+      form.key.m !== "" &&
+      form.key.b !== "") ||
+    (isHillKey(form.key, form.cipher) &&
+      form.key.matrix.filter((row) => {
+        const filteredRow = row.filter((entry) => entry === "");
+        return filteredRow.length > 0;
+      }).length === 0) ||
+    (isSuperKey(form.key, form.cipher) &&
+      form.key.vigenere !== "" &&
+      form.key.transposition !== "");
+
+  const isFormComplete =
+    isKeyCompleted &&
+    form.inputType === InputType.Text &&
+    form.textInput !== "";
+
+  const generateRequestKey = () => {
+    return isTextKey(form.key, form.cipher)
+      ? form.key
+      : isAffineKey(form.key, form.cipher)
+      ? {
+          m: parseInt(form.key.m),
+          b: parseInt(form.key.b),
+        }
+      : isSuperKey(form.key, form.cipher)
+      ? {
+          vigenere: form.key.vigenere,
+          transposition: parseInt(form.key.transposition),
+        }
+      : isHillKey(form.key, form.cipher)
+      ? form.key.matrix.map((row) => {
+          return row.map((entry) => parseInt(entry));
+        })
+      : "";
+  };
+
+  const handleEncryptClick = async () => {
+    try {
+      setCipherResult(null);
+      setPageStatus(PageStatus.Loading);
+
+      const textToSend =
+        form.textInputType === TextInputType.Text
+          ? form.textInput
+          : safeAtob(form.textInput);
+
+      const key = generateRequestKey();
+
+      const { data } = await axios.put<CipherResult>(
+        `${BE_URL}/cipher/${form.cipher}/encrypt/text`,
+        {
+          text: textToSend,
+          key,
+        }
+      );
+
+      setCipherResult(data);
+    } catch {
+      window.alert("Failed to perform encryption");
+    } finally {
+      setPageStatus(PageStatus.None);
+    }
+  };
+
+  const handleDecryptClick = async () => {
+    try {
+      setCipherResult(null);
+      setPageStatus(PageStatus.Loading);
+
+      const textToSend =
+        form.textInputType === TextInputType.Text
+          ? form.textInput
+          : safeAtob(form.textInput);
+
+      const key = generateRequestKey();
+
+      const { data } = await axios.put<CipherResult>(
+        `${BE_URL}/cipher/${form.cipher}/decrypt/text`,
+        {
+          text: textToSend,
+          key,
+        }
+      );
+
+      setCipherResult(data);
+    } catch {
+      window.alert("Failed to perform encryption");
+    } finally {
+      setPageStatus(PageStatus.None);
+    }
+  };
+
+  return {
+    form,
+    pageStatus,
+    cipherResult,
+    handleCipherChange,
+    handleTextKeyChange,
+    handleAffineKeyMChange,
+    handleAffineKeyBChange,
+    handleSuperKeyVigenereChange,
+    handleSuperKeyTranspositionChange,
+    handleHillKeyMatrixSizeChange,
+    handleHillKeyMatrixChange,
+    handleInputTypeChange,
+    handleTextInputTypeChange,
+    handleTextInputChange,
+    isFormComplete,
+    handleEncryptClick,
+    handleDecryptClick,
+  };
+};
